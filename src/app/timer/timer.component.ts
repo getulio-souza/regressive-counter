20f5ac02b76311a493059cd0df1b07af9dfe0555
getulio.souza <getulio.souza@evolutix.com.br>
2026-02-03 21:59:44 -0300

    feature: counter

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -3,32 +3,49 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit {
-
   counter: any;
-  timer: number = 10;
+  timer: any;
   showMessage: boolean = false;
   showTimer: boolean = true;
 
+  hours: number = 24;
+  minutes: number = 0;
+  seconds: number = 59;
+
   ngOnInit(): void {
+    this.counter = setInterval(() => {
 
-   this.counter = setInterval(()=> {
+      if (this.seconds > 0 && this.seconds < 60) {
+        this.seconds = this.seconds - 1;
+      }
 
-    this.timer = this.timer -1;
+      if (this.seconds === 0) {
+        this.minutes = this.minutes - 1;
+      }
 
-    if(this.timer <= 0){
-      this.stopCounter()
+      if (this.seconds === 0 && this.minutes === 0) {
+        this.hours = this.hours - 1;
+      }
 
-      this.showMessage = true;
-      this.showTimer = false;
-    }
-   },1000)
+    }, 1000);
+  }
 
-}
-
-  stopCounter(){
-    clearInterval(this.counter)
+  stopCounter() {
+    clearInterval(this.counter);
   }
 }
+
+//  this.counter = setInterval(()=> {
+
+//   this.timer = this.timer -1;
+
+//   if(this.timer <= 0){
+//     this.stopCounter()
+
+//     this.showMessage = true;
+//     this.showTimer = false;
+//   }
+//  },1000)
