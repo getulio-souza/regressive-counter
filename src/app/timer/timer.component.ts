4aa8004d9fefcd8056a4171772c3100358efa00b
Getulio Silva25 <gsilva25@lenovo.com>
2026-02-05 16:42:04 -0300

    refactor: counter-updated

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -11,7 +11,7 @@ export class TimerComponent implements OnInit {
   showMessage: boolean = false;
   showTimer: boolean = true;
 
-  hours: number = 24;
+  hours: number = 2;
   minutes: number = 0;
   seconds: number = 0;
 
@@ -19,24 +19,39 @@ export class TimerComponent implements OnInit {
 
     this.counter = setInterval(() => {
 
+      this.showMessage = false;
+
       if (this.seconds > 0) {
+        console.log('segundos é maior do que zero')
         this.seconds = this.seconds - 1;
       } else {
-        this.seconds = 59;
+        this.seconds = 2;
 
         if (this.minutes > 0) {
+          console.log('minutos é maior do que zero')
           this.minutes = this.minutes - 1;
         } else {
-          this.minutes = 59;
+          this.minutes = 2;
         }
 
-        if (this.minutes === 0 && this.seconds === 0) {
-          this.hours = this.hours - 1;
-        } else {
-          this.stopCounter()
-        }
-
+        
       }
+      
+      if(this.minutes === 0 && this.seconds === 0){
+        console.log('segundos e minutos são iguais a zero')
+        this.hours = this.hours - 1;
+      }
+      
+
+      if (this.hours === 0 && this.minutes === 0 && this.seconds === 0) {
+        console.log('contador chegou ao fim!')
+          this.stopCounter()
+
+          setTimeout(()=> {
+            this.showMessage = true;
+          },300)
+        }
+
     }, 1000);
   }
 
