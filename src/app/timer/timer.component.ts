3d63b3eedafbd483864ecdf772ae6e74baa408fa
getulio.souza <getulio.souza@evolutix.com.br>
2026-02-04 22:00:17 -0300

    feature: if-structure

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -13,23 +13,30 @@ export class TimerComponent implements OnInit {
 
   hours: number = 24;
   minutes: number = 0;
-  seconds: number = 59;
+  seconds: number = 0;
 
   ngOnInit(): void {
+
     this.counter = setInterval(() => {
 
-      if (this.seconds > 0 && this.seconds < 60) {
+      if (this.seconds > 0) {
         this.seconds = this.seconds - 1;
-      }
+      } else {
+        this.seconds = 59;
 
-      if (this.seconds === 0) {
-        this.minutes = this.minutes - 1;
-      }
+        if (this.minutes > 0) {
+          this.minutes = this.minutes - 1;
+        } else {
+          this.minutes = 59;
+        }
 
-      if (this.seconds === 0 && this.minutes === 0) {
-        this.hours = this.hours - 1;
-      }
+        if (this.minutes === 0 && this.seconds === 0) {
+          this.hours = this.hours - 1;
+        } else {
+          this.stopCounter()
+        }
 
+      }
     }, 1000);
   }
 
@@ -37,15 +44,3 @@ export class TimerComponent implements OnInit {
     clearInterval(this.counter);
   }
 }
-
-//  this.counter = setInterval(()=> {
-
-//   this.timer = this.timer -1;
-
-//   if(this.timer <= 0){
-//     this.stopCounter()
-
-//     this.showMessage = true;
-//     this.showTimer = false;
-//   }
-//  },1000)
